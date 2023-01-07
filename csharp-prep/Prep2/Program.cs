76acbbe34b6742f57271da31aae3144fdc15ec9f
ishimwezachee <zacheeishimwe@gmail.com>
2023-01-07 12:04:48 +0200

    leraning activity 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What percentage do you have? ");
+        int percentage = int.Parse(Console.ReadLine());
+
+        if(percentage>=90 )
+        {
+            Console.WriteLine("Your grade is A");
+        }
+        else if(percentage>=80)
+        {
+            Console.WriteLine("Your grade is B");
+        }
+        else if(percentage >= 70)
+        {
+            Console.WriteLine("Your grade is C");
+        }
+        else if(percentage>= 60)
+        {
+            Console.WriteLine("Your grade is D");
+        }
+        else
+        {
+            Console.WriteLine("Your grade is is F");
+        }
+        
     }
 }
